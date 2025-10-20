5d742249a9f1d4e9473d4ddd51bbddd6a58498e2
JamesCWong <jwong787@my.bcit.ca>
2025-10-20 02:17:29 -0700

    Updated frontend to have preset button

--- js/request.js
+++ js/request.js
@@ -9,12 +9,13 @@ class RequestHandler {
     sendQuery() {
         const query = document.getElementById('sql').value;
         const lowerQuery = query.trim().toLowerCase();
-
+        //Sends a GET for the select query
         if (lowerQuery.startsWith('select')) {
             fetch(`${this.backendUrl}/?q=${encodeURIComponent(query)}`)
                 .then(res => res.text())
                 .then(data => document.getElementById('response').textContent = data)
                 .catch(err => document.getElementById('response').textContent = err);
+        //Sends a POST for the insert query
         } else if (lowerQuery.startsWith('insert')) {
             fetch(this.backendUrl + '/', {
                 method: 'POST',
@@ -27,6 +28,27 @@ class RequestHandler {
         } else {
             document.getElementById('response').textContent = RESPONSE_ERROR;
         }
+    }
+
+    staticQuery() {
+        //The static query
+        const query = `
+            INSERT INTO patients (name, dateOfBirth) VALUES 
+            ('Sara Brown', '1901-01-01'),
+            ('John Smith', '1941-01-01'),
+            ('Jack Ma', '1961-01-30'),
+            ('Elon Musk', '1999-01-01');
+        `;
+
+        fetch(this.backendUrl + '/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({query})
+        })
+        .then(res = res.text())
+        //Display the response or error.
+        .then(data =>document.getElementById('presetResponse').textContent = data)
+        .catch(err => document.getElementById('response').textContent = err);
     }
 }
 
